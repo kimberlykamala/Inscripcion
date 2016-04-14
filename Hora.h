652aded5f1389d7cb20503bbfcbc3077fb6ecf95
kimberlykamala <kimberly_antequera@hotmail.com>
2016-04-14 10:16:34 -0400

    Create Hora.h

---
+++ Hora.h
@@ -0,0 +1,20 @@
+class Hora
+{
+	private:
+
+	int minutos;
+	int segundos;
+	int horas;
+public:
+	int getMinutos();
+	void setMinutos(int m);
+
+	int getSegundos();
+	void setSegundos(int s);
+
+	int getHoras();
+	void setHoras(int h);
+
+	Hora getObjetoHora();
+	void setObjetoHora(int h);
+}
