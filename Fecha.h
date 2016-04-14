e6468d6e938e2bd24c4b0d94bcc196b4b22045af
kimberlykamala <kimberly_antequera@hotmail.com>
2016-04-14 10:15:57 -0400

    Create Fecha.h

---
+++ Fecha.h
@@ -0,0 +1,20 @@
+class Fecha
+{
+private:
+
+	int dia;
+	int mes;
+	int año;
+public:
+	int getDia();
+	int setDia(int d);
+
+	int getMes();
+	int setMes(int m);
+
+	int getAño();
+	int setAño(int a);
+	
+	Fecha getFecha();
+	void setFecha(Fecha f);
+}
